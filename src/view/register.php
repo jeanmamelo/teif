41504e3248448813c7b240aa95c3dfb534817c83
Yuri Fernandes <contato@yurifernandes.com>
2021-03-13 23:12:05 -0300

    Register and login responsive screens

--- src/view/register.php
+++ src/view/register.php
@@ -13,15 +13,23 @@ require_once 'partials/head.php';
     <div class="container login-middleContainer d-flex flex-column justify-content-center">
         <div class="row">
             <div class="col-12 col-md-6 offset-md-3 d-flex">
-                <form class="box" id="loginForm">
-                    <h1>Login</h1>
-                    <p class="text-muted"> Já possiu uma conta? Logue-se </p>
+                <form class="box mx-auto" id="registerForm">
+                    <h1>Cadastro</h1>
+                    <p class="text-muted">Preencha os dados abaixo</p>
+
                     <label hidden for="username">Usuário</label>
                     <input type="text" name="username" id="username" placeholder="Usuário">
+
                     <label hidden for="password">Senha</label>
                     <input type="password" name="password" placeholder="Senha">
-                    <p class="text-muted">Ainda não possui conta? <a class="forgot text-muted" href="#"> Cadastre-se </a></p>
-                    <input type="submit" name="login" value="Login" href="#" form="loginForm">
+
+                    <label hidden for="passwordConfirmation">Confirmar senha</label>
+                    <input type="password" name="passwordConfirmation" placeholder="Confirmar senha">
+
+                    <label hidden for="email">E-mail</label>
+                    <input type="email" name="email" id="email" placeholder="E-mail">
+
+                    <input type="submit" name="register" value="Cadastrar" href="#" form="registerForm">
                 </form>
             </div>
         </div>
