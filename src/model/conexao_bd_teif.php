a0018582715ed21da3d7b53a269c32fdc502ead4
leonardo <leonardosouzac12@gmail.com>
2021-03-13 21:46:01 -0300

    Conexao com BD
    Código de conexão com o banco de dados, verificar username e password

---
+++ src/model/conexao_bd_teif.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername =  "localhost";
+$dbUsername = "admin";
+$dbPassword = "root";
+$dbName = "teif";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
