bd79d43bab44fe4a91e63105113da7abd6d0bafa
Jean Melo <jeanmamelo@gmail.com>
2021-03-14 16:31:59 -0300

    update login and register styles
    and some minor changes and fixes

--- src/view/login.php
+++ src/view/login.php
@@ -13,14 +13,19 @@ require_once 'partials/head.php';
     <div class="container login-middleContainer d-flex flex-column justify-content-center">
         <div class="row">
             <div class="col-12 col-md-6 offset-md-3 d-flex">
-                <form class="box" id="loginForm">
-                    <h1>Login</h1>
-                    <p class="text-muted"> Já possiu uma conta? Logue-se </p>
-                    <label hidden for="username">Usuário</label>
-                    <input type="text" name="username" id="username" placeholder="Usuário">
+                <form method="post" class="box mx-auto" id="loginForm" action="">
+                    <h1> Login </h1>
+
+                    <p> Já possiu uma conta? Logue-se </p>
+
+                    <label hidden for="cpf">CPF</label>
+                    <input type="text" name="cpf" id="cpf" placeholder="CPF" pattern="[0-9]{11}" minlength="11" maxlength="11" required>
+
                     <label hidden for="password">Senha</label>
-                    <input type="password" name="password" placeholder="Senha">
-                    <p class="text-muted">Ainda não possui conta? <a class="forgot text-muted" href="#"> Cadastre-se </a></p>
+                    <input type="password" name="password" placeholder="Senha" maxlength="30" required>
+
+                    <p> Ainda não possui conta? <a class="under" href="register"> Cadastre-se </a></p>
+
                     <input type="submit" name="login" value="Login" href="#" form="loginForm">
                 </form>
             </div>
